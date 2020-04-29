dcb4189733cfe3b3dff2f87ce6cf9dc5bb109993
mohankumar1u <mohankumar.uppu@gmail.com>
2020-04-29 23:43:06 +0530

    3 files modified

--- src/app/single-country/single-country.component.ts
+++ src/app/single-country/single-country.component.ts
@@ -13,7 +13,7 @@ export class SingleCountryComponent implements OnInit {
   countries:any;
   selectedCountry:any;
   darkmode:string;
-  cardborders:any;
+  borders=[];
 
 
   constructor(private activatedRoute: ActivatedRoute,private countriesService: CountriesService, private router: Router) { }
@@ -21,12 +21,30 @@ export class SingleCountryComponent implements OnInit {
   onBackToCountries(){
     this.router.navigate(['/countries']);
   }
+  async goToBorderCountry(border){
+    console.log("border")
+    await this.router.navigate(['/SingleCountryDetails',border.id]);
+     this.selectedCountry = this.countries[border.id];
+     this.borders=[];
+     this.ngOnInit();
+  }
 
   ngOnInit() {
       this.countriesService.countriesData.subscribe(countries=> this.countries = countries)
     let id = this.activatedRoute.snapshot.paramMap.get('id');
-    console.log(this.countries[id],"id");
+    console.log(this.countries[id],id);
     this.selectedCountry = this.countries[id];
+    for(let i=0;i<this.selectedCountry.borders.length; i++){
+       for (let [key, value] of Object.entries(this.countries)) {
+       if(this.selectedCountry.borders[i]==value.alpha3Code){
+         value["id"]=key;
+         this.borders.push(value);  
+       }
+        //obj.push(value.region);
+        // Object.assign(this.region , {key: value.region});
+      }
+    }
+    console.log(this.borders);
       
   }
 
